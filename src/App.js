8e1ecb120b20f0242d92715502d85b69597f11d7
YoannPitance <pittyo88@gmail.com>
2023-05-10 17:54:37 +0200

    2- mise en place des backgrounds et des jours

--- src/App.js
+++ src/App.js
@@ -1,5 +1,62 @@
+import React from "react";
+
+const api = {
+  key: "cd073fd69725f8325798b3c958e3559a",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+
 function App() {
-  return <div className="App"></div>;
+  const dateBuilder = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+  };
+
+  return (
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="Search..." />
+        </div>
+        <div className="location-box">
+          <div className="location">New York City, US</div>
+          <div className="date">{dateBuilder(new Date())} </div>
+        </div>
+        <div className="weather-box">
+          <div className="temp">15°</div>
+          <div className="weather">Sunny</div>
+          // https://www.youtube.com/watch?v=GuA0_Z1llYU 17"
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
